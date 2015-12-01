3322153d6564e9bab8d69f87b2136d399f50e6a0
usomax <qlast.gm@gmail.com>
2015-12-02 00:27:11 +0900

    初投稿です

---
+++ assets/sp/js/index.js
@@ -0,0 +1,26 @@
+
+/*!
+ * index.js - サイト名（●●のスクリプト）
+ * 動作環境： iPad / iPhone / Android
+ * 対応ブラウザ： 標準ブラウザ / GoogleChrome
+ * --------------------
+ * @version: 1.0
+ * @author :
+ * --------------------
+ */
+
+(function() {
+  (function($) {
+    $('#sample').on('click', function() {
+      console.log('hogehoge');
+      return false;
+    });
+    if (smartphoneType[3] === "iphone") {
+      return console.log('iphone');
+    } else {
+      return console.log('android');
+    }
+  })(jQuery);
+
+}).call(this);
+
