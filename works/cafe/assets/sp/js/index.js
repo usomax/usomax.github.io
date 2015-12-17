4d848faf2f1ce562788b19873ad6b9fc0aa09c46
usomax <qlast.gm@gmail.com>
2015-12-17 16:04:18 +0900

    sPresso改で更新

--- works/cafe/assets/sp/js/index.js
+++ works/cafe/assets/sp/js/index.js
@@ -10,32 +10,34 @@
  */
 
 (function() {
+  (function() {
     $(function() {
-        // 設定
-        var $width = 780; // 横幅
-        var $height = 250; // 高さ
-        var $interval = 5000; // 切り替わりの間隔（ミリ秒）
-        var $fade_speed = 1000; // フェード処理の早さ（ミリ秒）
-        $("#slide ul li").css({
-            "position": "relative",
-            "overflow": "hidden",
-            "width": $width,
-            "height": $height
-        });
-        $("#slide ul li").hide().css({
-            "position": "absolute",
-            "top": 0,
-            "left": 0
-        });
-        $("#slide ul li:first").addClass("active").show();
-        setInterval(function() {
-            var $active = $("#slide ul li.active");
-            var $next = $active.next("li").length ? $active
-                .next("li") : $("#slide ul li:first");
-            $active.fadeOut($fade_speed).removeClass(
-                "active");
-            $next.fadeIn($fade_speed).addClass("active");
-        }, $interval);
+      var $fade_speed, $height, $interval, $width;
+      $width = 780;
+      $height = 250;
+      $interval = 5000;
+      $fade_speed = 1000;
+      $('#slide ul li').css({
+        'position': 'relative',
+        'overflow': 'hidden',
+        'width': $width,
+        'height': $height
+      });
+      $('#slide ul li').hide().css({
+        'position': 'absolute',
+        'top': 0,
+        'left': 0
+      });
+      $('#slide ul li:first').addClass('active').show();
+      setInterval((function() {
+        var $active, $next;
+        $active = $('#slide ul li.active');
+        $next = $active.next('li').length ? $active.next('li') : $('#slide ul li:first');
+        $active.fadeOut($fade_speed).removeClass('active');
+        $next.fadeIn($fade_speed).addClass('active');
+      }), $interval);
     });
+  }).call(this);
+
 }).call(this);
 
