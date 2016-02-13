c8f0cefa4b72f174c0c3c3a5d19ac159be8613e2
usomax <qlast.gm@gmail.com>
2016-02-13 18:45:48 +0900

    久々の更新

--- assets/pc/js/index.js
+++ assets/pc/js/index.js
@@ -10,13 +10,45 @@
  */
 
 (function() {
+  var $box, $height, $width, zoom;
+
   (function($) {
     if (browserType[1] === "ie8" || browserType[1] === "ie7") {
 
     } else {
       return console.log('index: modern');
     }
-  })(jQuery);
+  }, $box = $(".box"), $height = $box.height(), $width = $box.width(), zoom = 1.5, $box.on({
+    mouseenter: function() {
+      $(this).find("img").stop().animate({
+        top: -$height / 3,
+        left: -$width / 3,
+        width: $width * zoom,
+        height: $height * zoom
+      });
+      return $(this).find(".overlay").stop().animate({
+        opacity: 1
+      });
+    },
+    mouseleave: function() {
+      $(this).find("img").stop().animate({
+        top: 0,
+        left: 0,
+        width: $width,
+        height: $height
+      });
+      return $(this).find(".overlay").stop().animate({
+        opacity: 0
+      });
+    }
+
+    /*$("li").on
+      mouseenter: ->
+        $(this).addClass "mOver"
+      mouseleave: ->
+        $(this).removeClass "mOver"
+     */
+  }))(jQuery);
 
 }).call(this);
 
