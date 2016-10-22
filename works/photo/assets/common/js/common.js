9b4ff27b54b2ec8a0ffcfb0e48b28d3d8c03c362
usomax <qlast.gm@gmail.com>
2016-10-22 17:41:18 +0900

    common.js更新

--- works/photo/assets/common/js/common.js
+++ works/photo/assets/common/js/common.js
@@ -13,6 +13,7 @@
   var _isMobile, _mo, _pl, _ua, dataUriInfo, screenInfo, selector, selector_n, userAgent;
 
   window.SITE_URL = location.protocol + '//' + location.host + '/works/photo/';
+  /*更新*/
 
   window.ASSETS_DIR = SITE_URL + 'assets/';
 
