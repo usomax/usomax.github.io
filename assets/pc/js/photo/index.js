fea6ca22f0d0982b909fa0abde38f3824d0b4044
usomax <qlast.gm@gmail.com>
2016-10-29 17:45:24 +0900

    パーティクル追加

--- assets/pc/js/photo/index.js
+++ assets/pc/js/photo/index.js
@@ -329,8 +329,92 @@
     $('#box').on('click', function() {
       $(this).fadeOut();
     });
-    return $('#inbox').on('click', function(e) {
+    $('#inbox').on('click', function(e) {
       event.stopPropagation();
+    });
+    return particlesJS('particles-js', {
+      'particles': {
+        'number': {
+          'value': 80,
+          'density': {
+            'enable': true,
+            'value_area': 800
+          }
+        },
+        'color': {
+          'value': '#ddd'
+        },
+        'shape': {
+          'type': 'circle',
+          'stroke': {
+            'width': 0,
+            'color': '#000000'
+          },
+          'polygon': {
+            'nb_sides': 5
+          },
+          'image': {
+            'src': 'img/github.svg',
+            'width': 100,
+            'height': 100
+          }
+        },
+        'opacity': {
+          'value': 0.5,
+          'random': false,
+          'anim': {
+            'enable': false,
+            'speed': 1,
+            'opacity_min': 0.1,
+            'sync': false
+          }
+        },
+        'size': {
+          'value': 3,
+          'random': true,
+          'anim': {
+            'enable': false,
+            'speed': 40,
+            'size_min': 0.1,
+            'sync': false
+          }
+        },
+        'line_linked': {
+          'enable': true,
+          'distance': 150,
+          'color': '#000',
+          'opacity': 0.1,
+          'width': 1
+        },
+        'move': {
+          'enable': true,
+          'speed': 6,
+          'direction': 'none',
+          'random': false,
+          'straight': false,
+          'out_mode': 'out',
+          'bounce': false,
+          'attract': {
+            'enable': false,
+            'rotateX': 600,
+            'rotateY': 1200
+          }
+        }
+      },
+      'interactivity': {
+        'detect_on': 'canvas',
+        'events': {
+          'onhover': {
+            'enable': false,
+            'mode': 'repulse'
+          },
+          'onclick': {
+            'enable': false,
+            'mode': 'push'
+          },
+          'resize': true
+        }
+      }
     });
   })(jQuery);
 
