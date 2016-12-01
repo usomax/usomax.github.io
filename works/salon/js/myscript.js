f25e2ca62dfae9be89b370b22cfd19662b3db13c
usomax <qlast.gm@gmail.com>
2016-12-01 13:59:44 +0900

    salon更新

--- works/salon/js/myscript.js
+++ works/salon/js/myscript.js
@@ -1,5 +1,31 @@
 $(function(){
-
+  showFlag = false;
+    topBtn = $('#page-top');
+    topBtn.css('opacity', '0');
+    showFlag = false;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 300) {
+        if (showFlag === false) {
+          showFlag = true;
+          topBtn.stop().animate({
+            'opacity': '1'
+          }, 200);
+        }
+      } else {
+        if (showFlag) {
+          showFlag = false;
+          topBtn.stop().animate({
+            'opacity': '0'
+          }, 200);
+        }
+      }
+    });
+    topBtn.click(function() {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 500);
+      return false;
+    });
     $('.slides').slick({
       centerMode: true,
       dots: true,
